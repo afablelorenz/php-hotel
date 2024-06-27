dd9a878ea2c9728266d945c95452e72c176b6cfa
afablelorenz <afablelorenz@gmail.com>
2024-06-27 16:12:53 +0200

    Finished milestone 1

--- index.php
+++ index.php
@@ -13,22 +13,57 @@ include_once __DIR__ ."/partials/hotel.php";
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Hotels</h1>
-        <ul>
-            <?php
-                foreach ($hotels as $hotel){
-            ?>
-            <?php echo $hotel['name']; ?>
-            <li><?php echo $hotel['description'] ?></li>
-            <li><?php echo $hotel['parking'] ?></li>
-            <li><?php echo $hotel['vote'] ?></li>
-            <li><?php echo $hotel['distance_to_center'] ?></li>
-            <?php
-                };
-            ?>
-        </ul>
-        
+        <div class="container">
+            <h1 class="my-4">
+                Hotels
+            </h1>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Parking</th>
+                        <th>Vote</th>
+                        <th>Distance to Center</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($hotels as $hotel){
+                    ?>
+                        <tr>
+                            <td>
+                                <?php
+                                    echo $hotel['name'];
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    echo $hotel['description'];
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    echo $hotel['parking'] ? 'Yes' : 'No';
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    echo $hotel['vote'];
+                                ?>
+                            </td>
+                            <td>
+                                <?php
+                                    echo $hotel['distance_to_center'];
+                                ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </body>
 </html>
